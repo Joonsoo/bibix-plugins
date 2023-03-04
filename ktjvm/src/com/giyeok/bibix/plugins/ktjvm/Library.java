3cfb4ab3e7e338127767b21272a528d8f20abad8
Joonsoo Jeon <giyeok7@gmail.com>
2023-03-04 18:03:10 +0900

    resources added to ktjvm

--- ktjvm/src/com/giyeok/bibix/plugins/ktjvm/Library.java
+++ ktjvm/src/com/giyeok/bibix/plugins/ktjvm/Library.java
@@ -6,19 +6,58 @@ import org.jetbrains.kotlin.cli.common.ExitCode;
 import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Library {
+    Set<Path> allFilesOf(Path directory) {
+        try {
+            return Files.list(directory).flatMap(sub -> {
+                if (Files.isDirectory(sub)) {
+                    return allFilesOf(sub).stream();
+                } else {
+                    return Stream.of(sub);
+                }
+            }).collect(Collectors.toSet());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private Set<Path> findResourceDirectoriesOf(Collection<Path> paths) {
+        List<Path> mutPaths = new ArrayList<>(paths);
+        Set<Path> resDirs = new HashSet<>();
+
+        while (!mutPaths.isEmpty()) {
+            Path path = mutPaths.remove(0);
+            Path directory = path.getParent();
+            Set<Path> dirFiles = allFilesOf(directory);
+            if (paths.containsAll(dirFiles)) {
+                resDirs.removeIf(it -> it.startsWith(directory));
+                resDirs.add(directory);
+                mutPaths.removeAll(dirFiles);
+            } else {
+                resDirs.add(path);
+            }
+        }
+        return resDirs;
+    }
+
     private BibixValue runCompiler(SetValue classPaths, SetValue deps, BuildContext context, ListValue optIns) throws IOException {
         Path destDirectory = context.getDestDirectory();
 
         SetValue srcs = (SetValue) context.getArguments().get("srcs");
         if (srcs.getValues().isEmpty()) {
             throw new IllegalArgumentException("srcs must not be empty");
+        }
+
+        SetValue resourcesValue = (SetValue) context.getArguments().get("resources");
+        Set<Path> resources = new HashSet<>();
+        for (BibixValue value : resourcesValue.getValues()) {
+            resources.add(((FileValue) value).getFile().toAbsolutePath());
         }
 
         if (context.getHashChanged()) {
@@ -55,6 +94,8 @@ public class Library {
             }
         }
 
+        Set<Path> resDirs = findResourceDirectoriesOf(resources);
+
         // ClassPkg = (origin: ClassOrigin, cpinfo: CpInfo, deps: set<ClassPkg>)
         // CpInfo = {JarInfo, ClassesInfo}
         // ClassesInfo = (classDirs: set<directory>, resDirs: set<directory>, srcs: {set<file>, none})
@@ -65,7 +106,7 @@ public class Library {
                 )),
                 "cpinfo", new NClassInstanceValue("jvm.ClassesInfo", Map.of(
                         "classDirs", new SetValue(new DirectoryValue(destDirectory)),
-                        "resDirs", new SetValue(),
+                        "resDirs", new SetValue(resDirs.stream().map(PathValue::new).collect(Collectors.toList())),
                         "srcs", srcs
                 )),
                 "deps", deps
@@ -75,7 +116,7 @@ public class Library {
     public BuildRuleReturn build(BuildContext context) throws IOException {
         SetValue deps = (SetValue) context.getArguments().get("deps");
         BibixValue optInsValue = (BibixValue) context.getArguments().get("optIns");
-        ListValue optIns = (optInsValue instanceof NoneValue)? null : (ListValue) optInsValue;
+        ListValue optIns = (optInsValue instanceof NoneValue) ? null : (ListValue) optInsValue;
         StringValue sdkVersion = (StringValue) context.getArguments().get("sdkVersion");
 
         return BuildRuleReturn.evalAndThen(
