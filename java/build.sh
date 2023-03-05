b861058e571113e0e5f41bb49b6e5ec1288fba2d
Joonsoo Jeon <giyeok7@gmail.com>
2023-03-05 22:11:54 +0900

    java plugin updated

--- java/build.sh
+++ java/build.sh
@@ -1,2 +1,2 @@
-kotlinc src/com/giyeok/bibix/plugins/java/JvmTypes.kt src/com/giyeok/bibix/plugins/java/Library.kt -cp ./libs/bibix-base-0.1.0-all.jar -d build/bibix-java-plugin.jar
+kotlinc src/com/giyeok/bibix/plugins/java/JvmTypes.kt src/com/giyeok/bibix/plugins/java/Library.kt -cp ./libs/bibix-base-0.2.1-all.jar -d build/bibix-java-plugin.jar
 
