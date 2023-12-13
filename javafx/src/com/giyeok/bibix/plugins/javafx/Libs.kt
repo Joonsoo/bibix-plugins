d8e06d7cc628f9a88379a1e8c8916003d7d8e27a
Joonsoo Jeon <giyeok7@gmail.com>
2023-12-13 13:10:30 +0900

    javafx updated

--- javafx/src/com/giyeok/bibix/plugins/javafx/Libs.kt
+++ javafx/src/com/giyeok/bibix/plugins/javafx/Libs.kt
@@ -1,8 +1,33 @@
 package com.giyeok.bibix.plugins.javafx
 
 import com.giyeok.bibix.base.*
+import java.nio.file.Path
 
 class Libs {
+  fun classPkgFrom(libDir: Path, moduleName: String): ClassInstanceValue {
+    val jarFile = libDir.resolve("javafx.$moduleName.jar")
+    return ClassInstanceValue(
+      "com.giyeok.bibix.plugins.jvm",
+      "ClassPkg",
+      mapOf(
+        "origin" to ClassInstanceValue(
+          "com.giyeok.bibix.plugins.jvm",
+          "LocalLib",
+          mapOf("path" to PathValue(jarFile))
+        ),
+        "cpinfo" to ClassInstanceValue(
+          "com.giyeok.bibix.plugins.jvm",
+          "JarInfo",
+          mapOf(
+            "jar" to FileValue(jarFile),
+          )
+        ),
+        "deps" to SetValue(),
+        "nativeLibDirs" to SetValue(DirectoryValue(libDir))
+      )
+    )
+  }
+
   fun build(context: BuildContext): BuildRuleReturn {
     val javafxVersion = (context.arguments.getValue("javafxVersion") as StringValue).value
     return BuildRuleReturn.evalAndThen(
@@ -23,25 +48,25 @@ class Libs {
           val unzipDir = (unzipDirValue as DirectoryValue).directory
           val libDir = unzipDir.resolve("javafx-sdk-$javafxVersion").resolve("lib")
 
+          //  base: jvm.ClassPkg,
+          //  controls: jvm.ClassPkg,
+          //  fxml: jvm.ClassPkg,
+          //  graphics: jvm.ClassPkg,
+          //  media: jvm.ClassPkg,
+          //  swing: jvm.ClassPkg,
+          //  web: jvm.ClassPkg,
           BuildRuleReturn.value(
             ClassInstanceValue(
-              "com.giyeok.bibix.plugins.jvm",
-              "ClassPkg",
+              "com.giyeok.bibix.plugins.javafx",
+              "JavaFxModules",
               mapOf(
-                "origin" to ClassInstanceValue(
-                  "com.giyeok.bibix.plugins.jvm",
-                  "LocalLib",
-                  mapOf("path" to DirectoryValue(libDir))
-                ),
-                "cpinfo" to ClassInstanceValue(
-                  "com.giyeok.bibix.plugins.jvm",
-                  "ClassesInfo",
-                  mapOf(
-                    "classDirs" to SetValue(DirectoryValue(libDir)),
-                    "resDirs" to SetValue()
-                  )
-                ),
-                "deps" to SetValue(),
+                "base" to classPkgFrom(libDir, "base"),
+                "controls" to classPkgFrom(libDir, "controls"),
+                "fxml" to classPkgFrom(libDir, "fxml"),
+                "graphics" to classPkgFrom(libDir, "graphics"),
+                "media" to classPkgFrom(libDir, "media"),
+                "swing" to classPkgFrom(libDir, "swing"),
+                "web" to classPkgFrom(libDir, "web"),
               )
             )
           )
