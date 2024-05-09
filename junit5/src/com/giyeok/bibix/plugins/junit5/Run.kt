a51070a1f8427a7b2c0a4b74f61bb7d7eb99341f
Joonsoo Jeon <giyeok7@gmail.com>
2024-05-09 11:08:02 +0900

    junit5

--- junit5/src/com/giyeok/bibix/plugins/junit5/Run.kt
+++ junit5/src/com/giyeok/bibix/plugins/junit5/Run.kt
@@ -1,6 +1,9 @@
 package com.giyeok.bibix.plugins.junit5
 
 import com.giyeok.bibix.base.*
+import org.junit.platform.engine.TestExecutionResult
+import org.junit.platform.engine.discovery.DiscoverySelectors.selectClasspathRoots
+import org.junit.platform.launcher.TestIdentifier
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder
 import org.junit.platform.launcher.core.LauncherFactory
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener
@@ -12,20 +15,47 @@ class Run {
       "resolveClassPkgs",
       mapOf("classPkgs" to deps)
     ) { classPaths ->
-      val classPathsValue = (classPaths as DataClassInstanceValue).value as NamedTupleValue
-      val cps = (classPathsValue.getValue("cps") as SetValue).values.map {
+      val classPathsValue = classPaths as ClassInstanceValue
+      val cps = (classPathsValue.getField("cps") as SetValue).values.map {
         (it as PathValue).path
       }
-      val res = classPathsValue.getValue("res") as SetValue // set<path>
+      val res = classPathsValue.getField("res") as SetValue // set<path>
 
       val launcher = LauncherFactory.create()
       val discoveryRequest = LauncherDiscoveryRequestBuilder.request()
-        .selectors().build()
+        .selectors(selectClasspathRoots(cps.toSet()))
+        .build()
       val testPlan = launcher.discover(discoveryRequest)
 
-      launcher.registerTestExecutionListeners(object : SummaryGeneratingListener() {
+      launcher.registerTestExecutionListeners(object: SummaryGeneratingListener() {
+        override fun dynamicTestRegistered(testIdentifier: TestIdentifier?) {
+          super.dynamicTestRegistered(testIdentifier)
+
+          context.progressLogger.logInfo("Registered: $testIdentifier")
+        }
+
+        override fun executionStarted(testIdentifier: TestIdentifier?) {
+          super.executionStarted(testIdentifier)
+
+          context.progressLogger.logInfo("Started: $testIdentifier")
+        }
+
+        override fun executionSkipped(testIdentifier: TestIdentifier?, reason: String?) {
+          super.executionSkipped(testIdentifier, reason)
+
+          context.progressLogger.logInfo("Skipped: $testIdentifier")
+        }
+
+        override fun executionFinished(
+          testIdentifier: TestIdentifier?,
+          testExecutionResult: TestExecutionResult?
+        ) {
+          super.executionFinished(testIdentifier, testExecutionResult)
+
+          context.progressLogger.logInfo("Finished: $testIdentifier $testExecutionResult")
+        }
       })
-      launcher.execute(discoveryRequest)
+      launcher.execute(testPlan)
 
       BuildRuleReturn.done()
     }
